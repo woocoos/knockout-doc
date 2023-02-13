cab940b7c1f3e933df82ba0b134dbf68cc504769
tsingsun <21997272@qq.com>
2023-02-13 17:08:53 +0800

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,125 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'AdminX',
+  tagline: 'AdminX是一套企业级业务系统解决方案,旨在帮助企业快速实现业务开发.',
+  url: 'https://woocoos.github.io',
+  baseUrl: '/adminx',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'woocoos', // Usually your GitHub org/user name.
+  projectName: 'adminx', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh',
+    locales: ['zh'],
+    localeConfigs: {
+        cn: {
+          label: '简体中文',
+          direction: 'ltr',
+        }
+    }
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: './md',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/woocoos/adminx-doc/tree/master/md/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/tsingsun/woocoo/tree/master/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'AdminX',
+        logo: {
+          alt: 'WooCoo Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'guide',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/woocoos/adminx',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Get Started',
+                to: '/docs/quickstart',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/woocoo',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/woocoo',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/woocoo',
+              },
+            ],
+          }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Tsingsun Li. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ sidebars.js
@@ -0,0 +1,34 @@
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  // But you can create a sidebar manually
+  md: [  
+    'guide',
+    {
+      type: 'category',
+      label: '指南',
+      items: ['quickstart'],
+      collapsed: false,
+    },    
+    {
+      type: 'category',
+      label: '概念',
+      items: ['account'],
+      collapsed: false,
+    },
+  ],
+};
+
+module.exports = sidebars;
